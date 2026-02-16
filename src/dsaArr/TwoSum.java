81f88483448b2c435d2438d3bd2baff6e5712b6f
sainijhalak <sainijhalak015@gmail.com>
2026-02-16 21:13:19 +0530

    java

--- src/dsaArr/TwoSum.java
+++ src/dsaArr/TwoSum.java
@@ -1,4 +1,29 @@
 package dsaArr;
-
+import java.util.*;
 public class TwoSum {
+    public static int[] sum(int [] nums,int t){
+
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            int sums=nums[i]+nums[j];
+            if(sums==t) return new int[]{i+1,j+1};
+            if(sums>t){
+                j--;
+            }
+            if(sums<t){
+                i++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+
+    public static void main(String[] args) {
+        int []nums={1,5,7,14,21};
+        int t=35;
+        int[] res= sum(nums,t);
+        for(int i=0;i<res.length;i++){
+            System.out.println(res[i]);
+        }
+    }
 }

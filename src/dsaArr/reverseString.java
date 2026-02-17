57ae9868b33ec859e78313ee6261382b5b4c2d0f
sainijhalak <sainijhalak015@gmail.com>
2026-02-17 06:15:23 +0530

    java

---
+++ src/dsaArr/reverseString.java
@@ -0,0 +1,22 @@
+package dsaArr;
+
+public class reverseString {
+    public static StringBuilder sR(StringBuilder sb, int low, int high){
+        if(sb.length()==0 || sb.length()==1) {
+            return  sb;
+        }
+        char temp=sb.charAt(low);
+        sb.setCharAt(low,sb.charAt(high));
+        sb.setCharAt(high,temp);
+        return sR( sb,low+1,high-1);
+    }
+    public static void main(String[] args) {
+        String s="JHALAK";
+        int low=0;
+        int high=s.length()-1;
+        StringBuilder sb=new StringBuilder(s);
+        String ans=sR(sb,low,high);
+        System.out.println(ans);
+
+    }
+}
